cf9f45fd33b759121e94506212351c89f93943a9
Fernando Reséndiz <resendizbautista801@gmail.com>
2021-01-22 11:28:58 -0600

    Added header and character views

---
+++ src/template/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+    const view = `
+        <div class="header">
+            <div class="header__logo">
+                <h1>
+                    <a href="/">Schwifty</a>
+                </h1>
+            </div>
+            <nav class="header__nav">
+                <a href="#/about/">About</a>
+            </nav>
+        </div>
+    `
+    return view
+}
+
+export default Header
